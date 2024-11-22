e8c8fbcaaa6fa0a3c94833bd847d8e00227ce073
Bryony Black <ikow8b1w@students.codeinstitute.net>
2024-11-22 15:44:38 +0000

    Add placeholder questions for Hobbit level

---
+++ assets/js/script.js
@@ -0,0 +1,93 @@
+
+
+const hobbitQuestions = [
+    {question: "Which two dwarves were Thorin's nephews?",
+        answers: [
+            {text: "Balin and Dwalin", correct: false},
+            {text: "Oin and Oin," correct: false},
+            {text: "Kili and Fili", correct: true},
+            {text: "Dori and Nori", correct: true},
+        ]
+    },
+    {
+        question: "Question 2",
+        answers[ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 3",
+        answers[ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 4",
+        answers[ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 5",
+        answers[ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 6",
+        answers[ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 7",
+        answers[ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 8",
+        answers[ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 9",
+        answers[ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 10",
+        answers[ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    }
+]
