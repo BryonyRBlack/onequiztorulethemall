56d409581b43488063ebf76799e3f20411ea38af
Bryony Black <ikow8b1w@students.codeinstitute.net>
2024-11-22 15:47:48 +0000

    Add placeholder questions for LOTR setting

--- assets/js/script.js
+++ assets/js/script.js
@@ -91,3 +91,95 @@ const hobbitQuestions = [
         ]
     }
 ]
+
+const lotrQuestions = [
+    {question: "What was the name of the Spider that Sam killed?",
+        answers: [
+            {text: "Ungoliant", correct: false},
+            {text: "Faramir", correct: false},
+            {text: "Shelob", correct: true},
+            {text: "Azog", correct: true},
+        ]
+    },
+    {
+        question: "Question 2",
+        answers: [ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 3",
+        answers: [ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 4",
+        answers: [ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 5",
+        answers: [ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 6",
+        answers: [ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 7",
+        answers: [ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 8",
+        answers: [ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 9",
+        answers: [ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    },
+    {
+        question: "Question 10",
+        answers: [ 
+            {text: "Answer1", correct: false},
+            {text: "Answer2", correct: false},
+            {text: "Answer3", correct: true},
+            {text: "Answer4", correct: false},
+        ]
+    }
+]
